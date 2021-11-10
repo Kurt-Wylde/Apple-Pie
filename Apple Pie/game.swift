874ab20f05a01a783564453a7bf362a56dc8ed4f
Evgeny Koshkin <wylde.kurt@gmail.com>
2021-11-10 21:19:28 +0300

    stable, playable

---
+++ Apple Pie/game.swift
@@ -0,0 +1,32 @@
+//
+//  game.swift
+//  Apple Pie
+//
+//  Created by Evgeny Koshkin on 10.11.2021.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
+    
+    mutating func playerGuessed(letter: Character) {
+        guessedLetters.append(letter)
+        if !word.contains(letter) {
+            incorrectMovesRemaining -= 1
+        }
+    }
+    var formattedWord: String {
+        var guessedWord = ""
+        for letter in word {
+            if guessedLetters.contains(letter) {
+                guessedWord += "\(letter)"
+            } else {
+                guessedWord += "_"
+            }
+        }
+        return guessedWord
+    }
+}
